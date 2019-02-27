25b8274c743229ef7b28446a8ac082eaa2476ae4
DaniAffCH <danieleaffinita2000@gmail.com>
2019-02-27 19:42:21 +0100

    bug fix, add reset bttn

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
-let widthCanvas = heightCanvas = 400;
+let widthCanvas = heightCanvas = 500;
 let radius = widthCanvas/2;
 var count = 0;
-let total = 10000;
+var total = null;
 var dentro = 0;
 var output;
-function setup() {
-    let cnv = createCanvas(widthCanvas, heightCanvas);
-    /* cnv.position(width/2, 0); */
+condizioniIniziali =()=>{
     background(0);
     translate(radius, radius);
     stroke(255, 255, 255, 255);
@@ -15,28 +13,57 @@ function setup() {
     ellipse(0, 0, widthCanvas, heightCanvas);
     rectMode(CENTER);
     rect(0,0, widthCanvas, heightCanvas);
+}
+function setup() {
+    createCanvas(widthCanvas, heightCanvas);
+    condizioniIniziali();
     output = createElement("h2", "");
 }
 
 function draw() {
-    if(count < total){
-        count++;
+    if(count < total && total != null){
+        //traslazione dei punti
         translate(radius, radius);
+
         getRandomPoint = () =>{
             x = random(-radius, radius)
             y = random(-radius, radius)
             //teorema di pitagora per la distanza
-            distanza = sqrt(pow(x,2)+pow(y,2));
+            distanza = sqrt(pow(x, 2)+pow(y, 2));
             if(distanza < radius){
+                //verde
                 stroke(0, 255, 0);
                 dentro++;
             } else {
+                //rosso
                 stroke(255, 0, 0);
             }
+            strokeWeight(1)
             point(x, y);
         }
-        getRandomPoint();
-        myPi = 4 * (float(dentro)/count);
-        output.html(myPi)
+
+        for(c = 0; c<=2500; c++){
+            if(count < total){
+                count++;
+                getRandomPoint();
+                myPi = 4 * (dentro/count);
+                output.html(myPi);
+            }
+            else{
+                break;
+            }
+        }
     }
+    else if(total === null){
+        //jquery per il recupero dati
+        var form = $("#inputForm");
+        var inputBox = $("#n");
+        var submitButton = $("#submitButton");
+        submitButton.click(function(){
+            total = $("#n").val();
+        });
+    }
+    //reset
+    var resetButton = $("#resetButton");
+    resetButton.click(function(){ location.reload();});
 }
